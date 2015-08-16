1adb16415bfd1969ca82e3c7438b1c90660d3899
Alec Hewitt <alec.hewitt9@gmail.com>
2015-08-16 12:49:16 +0100

    Circles dissapearing with matrix transformation for animation

--- computer_graphics_book/my_circles_disappearing_animated/Affine.js
+++ computer_graphics_book/my_circles_disappearing_animated/Affine.js
@@ -1,7 +1,7 @@
 /**
- * Represents a 2d transformation matrix column major orientation:
+ * Represents a 2d transformation matrix in column major orientation:
  *
- *   a  b  0
+ C*   a  b  0
  *   c  d  0
  *   e  f  1
  *
@@ -14,16 +14,19 @@
  * This represents a transformation of the coordinates (x, y) to
  * ( a*x + c*y + e, b*x + d*y + e )
  */
-var AffineTransformation = function(){
-    this.a = 1 ;
+var Circle = function (color) {
+    this.a = 1;
     this.b = 0;
     this.c = 0;
     this.d = 1;
     this.e = 0;
     this.f = 0;
+
+    this.color = color;
+    this.erased = false;
 };
 
-AffineTransformation.prototype.getMat3 = function() {
+Circle.prototype.getMat3 = function () {
     return [
         this.a, this.b, 0,
         this.c, this.d, 0,
@@ -31,14 +34,36 @@ AffineTransformation.prototype.getMat3 = function() {
     ];
 };
 
-AffineTransformation.prototype.translate = function(dx, dy) {
+Circle.prototype.isErased = function () {
+    return this.erased;
+};
+
+Circle.prototype.getColor = function () {
+    return this.color;
+};
+
+Circle.prototype.translate = function (dx, dy) {
     this.e += dx;
     this.f += dy;
 };
 
-AffineTransformation.prototype.scale = function(scaleFactor) {
+
+Circle.prototype.scale = function (scaleFactor) {
     this.a *= scaleFactor;
     this.b *= scaleFactor;
     this.c *= scaleFactor;
     this.d *= scaleFactor;
 };
+
+Circle.prototype.getScaleFactor = function () {
+    return this.a;
+};
+
+Circle.prototype.getCenterX = function () {
+    return this.e
+};
+
+Circle.prototype.getCenterY = function () {
+    return this.f;
+};
+
